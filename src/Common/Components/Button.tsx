780b806f13bae983644b2c606a4db48e313beda5
Nike768 <nikhilabc768@gmail.com>
2025-05-12 22:47:23 +0530

    variant of button component corrected

--- src/Common/Components/Button.tsx
+++ src/Common/Components/Button.tsx
@@ -4,7 +4,7 @@ import type { JSX } from "react";
 interface ButtonProps {
     btnText: string; // Text to be displayed on the button
     onClickhandler: Function; // Callback function to be called when the button is clicked 
-    variant: string; // Variant of the button (e.g., "contained", "outlined", etc.)
+    variant: "text" | "contained" | "outlined"; // Variant of the button (e.g., "contained", "outlined", etc.)
     disabled?: boolean; // It will enable/disable the button based on the value pased to disabled prop
     btnIcon?: JSX.Element; // Icon to be displayed on the button
 }
